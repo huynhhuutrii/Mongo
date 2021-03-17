109b5125138f08d8a7aee73a1f6912ce70e90121
Huỳnh Trí <huynhtri2k11@gmail.com>
2021-03-17 13:10:53 +0700

    first

---
+++ sever.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.connect(
+  MONGO_URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('Connected to MongoDb');
+  }
+);
